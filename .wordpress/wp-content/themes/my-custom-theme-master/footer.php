e272d401d8b27097907f254174a758e86c76facf
kishan <joeko@hotmail.com>
2024-11-04 21:51:59 +0100

    wordpress

---
+++ .wordpress/wp-content/themes/my-custom-theme-master/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The footer template part.
+ *
+ * @copyright  Copyright (c) 2020, Danny Cooper
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
+		
+
+		wp_footer(); ?>
+		<footer>
+			
+		</footer>
+	</body>
+</html>
